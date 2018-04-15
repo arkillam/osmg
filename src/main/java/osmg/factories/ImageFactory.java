470cec0a0129f4299a24442933bfc598bdde8a88
Andrew Killam <arkillam@gmail.com>
2018-04-15 00:12:03 -0400

    working on image factory

--- src/main/java/osmg/factories/ImageFactory.java
+++ src/main/java/osmg/factories/ImageFactory.java
@@ -1,5 +1,12 @@
 package osmg.factories;
 
+import java.awt.Image;
+import java.util.HashMap;
+import java.util.Map;
+
+import osmg.enums.ChassisSizeEnum;
+import osmg.enums.DirectionEnum;
+
 /**
  * Provides images of mechs as needed. The minimum required is a 32x32 north-facing mech image - the factory can expand
  * that to 64x64 and 128x128 images, and rotate to get the four directions. Optionally, 64x64 and 128x128 images with
@@ -8,6 +15,31 @@ package osmg.factories;
 
 public class ImageFactory {
 
-public static final Image getImage ()
-	
+	/** caches images */
+	private Map<String, Image> cachedImages = new HashMap<>();
+
+	/**
+	 * @param uniqueImageId
+	 *            the image's unique id
+	 * @param chassisSize
+	 *            the chassis size
+	 * @param imageSize
+	 *            the image's size (32, 64, 128 etc pixels)
+	 * @param facing
+	 *            direction the mech is facing
+	 * @return the requested image
+	 */
+	public final Image getImage(int uniqueImageId, ChassisSizeEnum chassisSize, int imageSize, DirectionEnum facing) {
+
+		String imageKey = String.format("%s_%d_%d_%s", chassisSize.toString().toUpperCase(), imageSize, uniqueImageId,
+				facing.toString().toUpperCase());
+
+		if (cachedImages.containsKey(imageKey))
+			return cachedImages.get(imageKey);
+		
+		... 
+
+		return null;
+	}
+
 }
